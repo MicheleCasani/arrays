25ae8fc88ef304af3673128a334509dadb7aab24
MicheleCasani <casanimichele@gmail.com>
2025-03-19 15:06:13 +0100

    [UPD] DOne folder 01-arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -11,37 +11,47 @@ const teachers = [
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
 const fourthTeacher = teachers[3];
-console.log(fourthTeacher , teachers)
+console.log(fourthTeacher, teachers);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice(4,1,'patrick')
-console.log(teachers)
+teachers.splice(4, 1, 'patrick');
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers[7];
-teachers.pop()
-console.log(teachers , fourthTeacher)
+const lastTeacher = teachers[6];
+teachers.pop();
+console.log(teachers, lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers[0];
-teachers.shift()
-console.log(teachers , firstTeacher)
+teachers.shift();
+console.log(teachers, firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push('Vanessa')
-console.log(teachers)
+teachers.push('Vanessa');
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.unshift('Sarah')
-console.log(teachers)
+teachers.unshift('Sarah');
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
-
-
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+let lewisIndex = 0;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Lewis') {
+    lewisIndex = i;
+  
+  }
+}
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
-
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+let isTeachersEmpty = false;
+for (let i = 0; i < teachers.length; i++){
+  if(teachers[i] == null){
+    isTeachersEmpty = true
+  }
+}
+console.log(isTeachersEmpty)

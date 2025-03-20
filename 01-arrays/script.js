a7e656ab2061a9af5b7da52cf64c0028f0f1f1c2
MicheleCasani <casanimichele@gmail.com>
2025-03-20 09:43:35 +0100

    [FIX]

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -18,9 +18,8 @@ teachers.splice(4, 1, 'patrick');
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers[6];
-teachers.pop();
-console.log(teachers, lastTeacher);
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = teachers[0];
